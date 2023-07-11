447caf9bc384567c284666dd78138aea9710e8e0
anila-jawed <73032957+hashamuddin1@users.noreply.github.com>
2023-07-11 12:58:30 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
+require('./config/db');
+const fileUpload=require('express-fileupload');
+const userRouter = require('./router/userRoute')
+const doctorRouter = require('./router/doctorRoute')
+const scheduleRouter = require('./router/doctorSchedule')
+const storeRouter = require('./router/medicalStoreRoute')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+  useTempFiles:true
+}))
+app.use([userRouter,doctorRouter,scheduleRouter,storeRouter])
+
+app.listen(port, () => {
+  console.log(`Our Server is running at port ${port} in Development Environment`);
+});
